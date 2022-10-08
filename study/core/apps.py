40f72402605f306d0071fdd5753f4762fc1ca861
Dmitry G <sdprecovery@gmail.com>
2022-10-08 11:21:17 +0300

    core CoreConfig update

--- study/core/apps.py
+++ study/core/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    name = 'study.core'
+    verbose_name = _("Core")
+
+    def ready(self):
+        try:
+            import study.core.signals  # noqa F401
+        except ImportError:
+            pass
