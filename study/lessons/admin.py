e68e93a6d5f3000170cf60d340092f02f7cbfd90
Dmitry G <sdprecovery@gmail.com>
2022-10-08 14:24:13 +0300

    App lessons init CourseAdmin, LessonAdmin, ExerciseAdmin

--- study/lessons/admin.py
+++ study/lessons/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, Lesson, Exercise
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    pass
